d06d85e9ce5ab2a76c3dd8bdc4cf70622ca1338a
TomaRouki <thomas.de.sousa@outlook.fr>
2023-06-14 11:23:37 +0200

    feat: modification makefile for OS linux/windows & generate structure users with nestjs

--- e-travel-back/src/app.module.ts
+++ e-travel-back/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'e-travel',
+      autoLoadEntities: true,
+      synchronize: true, // Synchronise automatiquement la structure de la base de données avec les entités (NE PAS UTILISER EN PRODUCTION)
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
