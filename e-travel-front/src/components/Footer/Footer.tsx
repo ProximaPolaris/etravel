c1bafebc5e70858335d88434c7366d5caff7a89f
Redcroow <kilian.mvrl@gmail.com>
2023-06-18 17:47:14 +0200

    feat(dev) : create component footer

---
+++ e-travel-front/src/components/Footer/Footer.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import styles from '../../styles/components/Footer.module.scss';
+import git from '../../img/icons8-github.svg';
+import Image from 'next/image';
+
+const Footer = () => {
+    const router = useRouter();
+    return (
+        <div className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.widgets}>
+                    <div className={styles.widget}>
+                        <div className={styles.nav_menu}>
+                            <h4>About</h4>
+                        </div>
+                    </div>
+                    <div className={styles.widget}>
+                        <div className={styles.nav_menu}>
+                            <h4>Contact</h4>
+                        </div>
+                    </div>
+                    <div className={styles.widget}>
+                        <div className={styles.nav_menu}>
+                            <h4>Follow Us</h4>
+                            <div className={styles.icons}>
+                                <div className={styles.icon}>
+                                    <a target="_blank" href="#" rel="noopener">
+                                        <img alt="Twitter" src="https://company.trivago.com/wp-content/uploads/2022/05/twitter-ffffff.svg" width="30px" />
+                                    </a>
+                                </div>
+                                <div className={styles.icon}>
+                                    <a target="_blank" href="https://github.com/ProximaPolaris/etravel" rel="noopener">
+                                        <Image src={git} alt="github" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.footer_bottom}>
+                <div className={styles.container}>
+                    <div className={styles.copyright}>
+                        © Copyright 2023 E-TRAVEL - All rights reserved
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- e-travel-front/src/pages/index.tsx
+++ e-travel-front/src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Header from '../components/Header/Header';
 import styles from '@import/styles/pages/Index.module.scss'
+import git from '../img/icons8-github.svg';
+import Image from 'next/image';
+import Footer from '@import/components/Footer/Footer';
 
 const IndexPage = () => {
   return (
     <div className={styles.body}>
       <Header />
-      <div className={styles.footer}>
-
+      <div className={styles.content}>
+        <h1>test</h1>
       </div>
+      <Footer />
     </div>
   );
 };
