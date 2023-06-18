362403b3677096586a6cb5f78c116261988e2bb2
TomaRouki <thomas.de.sousa@outlook.fr>
2023-06-18 17:01:44 +0200

    feat: continent entities

--- e-travel-back/src/api/continents/entities/continent.entity.ts
+++ e-travel-back/src/api/continents/entities/continent.entity.ts
@@ -1,1 +1,10 @@
-export class Continent {}
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity('continents')
+export class Continent {
+  @PrimaryColumn({ length: 2 })
+  code: string;
+
+  @Column({ length: 255 })
+  name: string;
+}
