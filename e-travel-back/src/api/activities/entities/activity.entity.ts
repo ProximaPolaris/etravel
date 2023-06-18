fd5d97556fb7e161da7e6de9dfcb719170120ae4
TomaRouki <thomas.de.sousa@outlook.fr>
2023-06-18 16:46:31 +0200

    feat: activities entity

--- e-travel-back/src/api/activities/entities/activity.entity.ts
+++ e-travel-back/src/api/activities/entities/activity.entity.ts
@@ -1,1 +1,29 @@
-export class Activity {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('activities')
+export class Activity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'type_id' })
+  typeId: number;
+
+  @Column({ name: 'travel_id' })
+  travelId: number;
+
+  @Column({ name: 'map_url', length: 100 })
+  mapUrl: string;
+
+  @Column({ name: 'created_at', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Column({ name: 'deleted_at', nullable: true })
+  deletedAt: Date | null;
+}
