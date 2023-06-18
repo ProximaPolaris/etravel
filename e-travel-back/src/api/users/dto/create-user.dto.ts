bd0f11475b26e2f7083880394ecac411bdb1b2db
TomaRouki <thomas.de.sousa@outlook.fr>
2023-06-18 19:00:40 +0200

    feat: dto users

--- e-travel-back/src/api/users/dto/create-user.dto.ts
+++ e-travel-back/src/api/users/dto/create-user.dto.ts
@@ -1,12 +1,45 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsDate,
+  IsEmail,
+  IsOptional,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   surname: string;
+
+  @IsNotEmpty()
+  @IsDate()
   birthdate: Date;
-  countryId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  countryId: string;
+
+  @IsNotEmpty()
+  @IsString()
   city: string;
+
+  @IsNotEmpty()
+  @IsString()
   zipCode: string;
+
+  @IsNotEmpty()
+  @IsString()
   address: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
-  keyApi: string;
 }
--- e-travel-back/src/api/users/dto/update-user.dto.ts
+++ e-travel-back/src/api/users/dto/update-user.dto.ts
@@ -1,4 +1,39 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString, IsDate, IsEmail } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  surname?: string;
+
+  @IsOptional()
+  @IsDate()
+  birthdate?: Date;
+
+  @IsOptional()
+  @IsString()
+  countryId?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  zipCode?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  password?: string;
+}
