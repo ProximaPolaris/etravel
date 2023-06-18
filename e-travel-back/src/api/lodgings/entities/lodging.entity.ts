eb432255c586ec9689330766265238c0eb9fce35
TomaRouki <thomas.de.sousa@outlook.fr>
2023-06-18 16:59:06 +0200

    feat: lodgings entities

--- e-travel-back/src/api/lodgings/entities/lodging.entity.ts
+++ e-travel-back/src/api/lodgings/entities/lodging.entity.ts
@@ -1,1 +1,26 @@
-export class Lodging {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('lodgings')
+export class Lodging {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'travel_id' })
+  travelId: number;
+
+  @Column({ length: 100 })
+  address: string;
+
+  @Column({ name: 'created_at', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Column({ name: 'deleted_at', nullable: true })
+  deletedAt: Date | null;
+}
