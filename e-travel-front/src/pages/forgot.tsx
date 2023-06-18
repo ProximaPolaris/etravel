1d15f7c9618f4dce547f2c9529508114a515c77c
Redcroow <kilian.mvrl@gmail.com>
2023-06-18 16:15:06 +0200

    feat(dev) : get value enter in input

--- e-travel-front/src/pages/forgot.tsx
+++ e-travel-front/src/pages/forgot.tsx
@@ -1,40 +1,47 @@
+import React, { useState } from 'react';
 import Header from '../components/Header/Header';
 import styles from '../styles/pages/Forgot.module.scss';
 
 const ForgotPage = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    console.log(email);
+  };
+
   return (
     <>
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <Header />
-                <div className={styles.card}>
-                    <div className={styles.form}>
-                        <h2>Forgot Password</h2>
-                        <div className={styles.inputCard}>
-                            <label>Email</label>
-                            <input type="email" required/>
-                        </div>
-                        <input type="submit" className={styles.buttonCard} />
-                    </div>
-                </div>
-                <div className={styles.action_button}>
-                    <a href="/">
-                        <button>Back</button>
-                    </a>
-                </div>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Header />
+          <div className={styles.card}>
+            <div className={styles.form}>
+              <h2>Forgot Password</h2>
+              <div className={styles.inputCard}>
+                <label>Email</label>
+                <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <input type="submit" className={styles.buttonCard} onClick={handleSubmit} />
             </div>
-            <div className={styles.right_part}>
-                <h1>Le saviez-vous ?</h1>
-                <span>
-                    Marco Polo, le premier explorateur née au 13e siècle, 
-                    a vécu une incroyable aventure en Asie, découvrant des palais, 
-                    rencontrant des empereurs et rapportant des récits de dragons 
-                    et de merveilles exotiques. 
-                    Son voyage légendaire continue de nous inspirer à explorer le monde et à nourrir 
-                    notre imagination.
-                </span>
-            </div>
+          </div>
+          <div className={styles.action_button}>
+            <a href="/">
+              <button>Back</button>
+            </a>
+          </div>
         </div>
+        <div className={styles.right_part}>
+          <h1>Le saviez-vous ?</h1>
+          <span>
+            Marco Polo, le premier explorateur née au 13e siècle, 
+            a vécu une incroyable aventure en Asie, découvrant des palais, 
+            rencontrant des empereurs et rapportant des récits de dragons 
+            et de merveilles exotiques. 
+            Son voyage légendaire continue de nous inspirer à explorer le monde et à nourrir 
+            notre imagination.
+          </span>
+        </div>
+      </div>
     </>
   );
 };
--- e-travel-front/src/pages/login.tsx
+++ e-travel-front/src/pages/login.tsx
@@ -1,57 +1,64 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header/Header';
 import styles from '../styles/pages/Login.module.scss';
 
-// const [email, setEmail] = useState('');
-// const [password, setPassword] = useState('');
-
-// const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setEmail(e.target.value);
-// };
-
-// const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setPassword(e.target.value);
-// };
-
-// const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     // Code pour la validation de l'authentification
-// };
-
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
   return (
     <>
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <Header />
-                <div className={styles.card}>
-                    <div className={styles.form}>
-                        <h2>Sign in</h2>
-                        <div className={styles.inputCard}>
-                            <input type="text" required/>
-                            <span>Username</span>
-                            <i></i>
-                        </div>
-                        <div className={styles.inputCard}>
-                            <input type="password" required/>
-                            <span>Password</span>
-                            <i></i>
-                        </div>
-                        <div className={styles.linksCard}>
-                            <a href="forgot">Forgot Password</a>
-                            <a href="signup">Sign Up</a>
-                        </div>
-                        <input type="submit" className={styles.buttonCard} />
-                    </div>
-                </div>
-                <div className={styles.action_button}>
-                    <a href="/">
-                        <button>Back</button>
-                    </a>
-                </div>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Header />
+          <div className={styles.card}>
+            <div className={styles.form}>
+              <h2>Sign in</h2>
+              <div className={styles.inputCard}>
+                <input
+                  type="text"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <span>Username</span>
+                <i></i>
+              </div>
+              <div className={styles.inputCard}>
+                <input
+                  type="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <span>Password</span>
+                <i></i>
+              </div>
+              <div className={styles.linksCard}>
+                <a href="forgot">Forgot Password</a>
+                <a href="signup">Sign Up</a>
+              </div>
+              <input
+                type="submit"
+                className={styles.buttonCard}
+                onClick={handleSubmit}
+              />
             </div>
-            <div className={styles.image} />
+          </div>
+          <div className={styles.action_button}>
+            <a href="/">
+              <button>Back</button>
+            </a>
+          </div>
         </div>
+        <div className={styles.image} />
+      </div>
     </>
   );
 };
