0149e6bf35d458ff63d1f452a501d71455f8b811
Redcroow <kilian.mvrl@gmail.com>
2023-06-24 23:35:18 +0200

    feat(dev) : add url for header + add href on homepage + create new travel page

---
+++ e-travel-front/src/pages/newTravel.tsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header/Header';
+import styles from '../styles/pages/NewTravel.module.scss';
+
+const NewTravelPage = () => {
+    return (
+        <>
+            <div className={styles.container}>
+                <Header />
+                <div className={styles.content}>
+                    
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default NewTravelPage;
